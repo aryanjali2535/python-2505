30a73604383e8c8a1a41b9c54143dfe6e0b4d688
aryanjali2535 <neeradiaryanjali337@gmail.com>
2025-10-29 14:01:03 +0530

    8oct-variables

---
+++ 01_intro/demo.py
@@ -0,0 +1,26 @@
+# This is comment, meaning python interpreter will ignore this line
+# function : does a task 
+# we have print() -> printing on the std out 
+
+print(10)
+# print(a) # NameError: name 'a' is not defined
+print("""welcome""") # NameError: name 'welcome' is not defined
+
+today = "tuesday"
+print(today)
+
+day2moro = "wednesday"
+print(day2moro)
+
+# 2moro = "wednesday" # invalid syntax as it starts with digit 
+# print(2moro)
+
+# Improper way of using identifiers 
+x = "Ravi"
+y = 32
+z = "Python"
+
+# Proper way of using identifiers (recommended)
+student_name = "Ravi"
+student_age = 32
+student_course = "Python"
